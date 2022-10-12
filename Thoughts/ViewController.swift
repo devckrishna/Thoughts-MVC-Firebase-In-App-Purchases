7caf19dc1ce3bbf561644ea24a241df425f54a85
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-10-12 18:12:25 +0530

    Initial Commit

---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Dev C Krishna on 12/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
